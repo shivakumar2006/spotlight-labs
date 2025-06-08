52f428f5fff2019d747c1c68ac4c781d3e174978
shivakumar2006 <official.shivakumar06@gmail.com>
2025-06-08 05:43:10 +0530

    commit

---
+++ backend/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/resendlabs/resend-go"
+)
+
+func main() {
+	// load enviroment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading in env file")
+	}
+
+	r := gin.Default()
+	r.POST("send/email", func(c *gin.Context) {
+		type RequestBody struct {
+			Email string `json:"email"`
+			Link  string `json:"link"`
+		}
+
+		var body RequestBody
+		if err := c.ShouldBindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid body"})
+			return
+		}
+
+		apiKey := os.Getenv("re_iVPUddjy_3EtBhk2BiYpVCUnRGvnY1zMX")
+		if apiKey == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "API key is not found"})
+			return
+		}
+
+		client := resend.NewClient(apiKey)
+		params := &resend.SendEmailRequest{
+			From:    "onboarding@resend.dev",
+			To:      []string{body.Email},
+			Subject: "Confirm your signup",
+			Html:    fmt.Sprintf("<h2>Confirm your account</h2><p><a href='%s'>Click here to verify</a></p>", body.Link),
+		}
+
+		_, err := client.Emails.Send(params)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"message": "Email sent successfully"})
+	})
+	r.Run(":8080")
+}
